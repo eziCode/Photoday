6a7496cd0435d0abe29e6ee597209ca2376aa7db
eziCode <ezraakresh@gmail.com>
2024-02-07 17:16:59 -0600

    small changes

--- logic/login.js
+++ logic/login.js
@@ -2,4 +2,9 @@ function loginButtonTapped() {
     // TODO: Check against authentication db
     let username = document.getElementById("usernameField").value;
     let password = document.getElementById("passwordField").value;
+
+    let data = {
+        username: username,
+        password: password
+    };
 }
