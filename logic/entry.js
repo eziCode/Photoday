7f3e382a8de546f5b78c9bccf3635f5b85d01ca4
eziCode <ezraakresh@gmail.com>
2024-02-09 18:18:18 -0600

    created and styled capture photo button

--- logic/entry.js
+++ logic/entry.js
@@ -11,3 +11,8 @@ navigator.mediaDevices.getUserMedia({
 .catch(function(error) {
     console.error('Error accessing camera:', error);
 });
+
+function capturePhoto() {
+    let button = document.getElementById('capture-photo-button');
+    // Link button to backend and capture photo
+}
