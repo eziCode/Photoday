3b9d28cd506e9a6492f12c2edb69f2d72659556d
eziCode <ezraakresh@gmail.com>
2024-04-24 12:41:45 -0500

    finished profile page

---
+++ logic/profile.js
@@ -0,0 +1,12 @@
+function getNameFromField() {
+    const name = document.getElementById('name_text_field').value;
+    // update name in database
+}
+
+function getEmailFromField() {
+    const name = document.getElementById('email_text_field_input').value;
+}
+
+/*
+ON APP STARTUP LOAD PROFILE INFORMATION AND SET THE TEXT FIELD
+*/
